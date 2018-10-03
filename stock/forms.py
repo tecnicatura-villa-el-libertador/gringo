3525b2946d4f7db6178cb21d035322c061ef0323
ryacyna <ryacyna@gmail.com>
2018-10-03 20:49:17 -0300

    Agregamos filtros

--- stock/forms.py
+++ stock/forms.py
@@ -1,17 +1,19 @@
 #https://docs.djangoproject.com/en/2.1/topics/forms/
 # https://tutorial.djangogirls.org/es/django_forms/
 from django import forms
-from .models import Movimiento, Lote
+from .models import Movimiento, Lote, Campaña, Cultivo
 
 
 class FilterForm(forms.ModelForm):
     actividad__campaña__lote = forms.ModelChoiceField(label="Lote", queryset=Lote.objects.all())
+    actividad__campaña = forms.ModelChoiceField(label="Campaña", queryset=Campaña.objects.all())
+    actividad__campaña__cultivo = forms.ModelChoiceField(label="Cultivo", queryset=Cultivo.objects.all())
 
     class Meta:
         model =  Movimiento
-        fields=  ('producto', 'tipo',  'actividad', 'actividad__campaña__lote')
+        fields=  ('producto', 'tipo',  'actividad', 'actividad__campaña__lote',
+        'actividad__campaña', 'actividad__campaña__cultivo')  
 
-#'campaña','lotes','cultivos',
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
