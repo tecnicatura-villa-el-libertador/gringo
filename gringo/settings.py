4a60822887415e3aa6110a4671211f521da52cdb
Martin Gaitan <mgaitan@onapsis.com>
2018-11-21 20:52:49 -0300

    arreglamos el flujo de reset de contraseña

--- gringo/settings.py
+++ gringo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'stock',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'stock',
     'django_registration',
     'bootstrap4',
     'crispy_forms',
